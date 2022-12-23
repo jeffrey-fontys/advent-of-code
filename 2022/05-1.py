0c7cacd52a8a754f9911376cddfb30cf4078c0d7
Jeffrey Derksen <470568@student.fontys.nl>
2022-12-23 20:38:42 +0100

    2022, day 5.

---
+++ 2022/05-1.py
@@ -0,0 +1,38 @@
+# Advent of Code 2022
+# Day 5: Supply Stacks
+# Part One
+# Made by Jeffrey Derksen
+
+matrix = []
+stack_amount = 0
+instructions = []
+with open('./2022/inputs/5.txt') as f:
+    for line in f:
+        if '[' in line:
+            matrix.append(line.strip('\n'))
+        elif line[0] == ' ' and stack_amount == 0:
+            stack_amount = int(line.strip().split()[-1])
+        elif line[0] == 'm':
+            line_split = line.strip().split()
+            instructions.append([
+                int(line_split[1]),
+                int(line_split[3]) - 1,
+                int(line_split[5]) - 1
+            ])
+
+stacks = []
+for i in range(stack_amount):
+    stack = []
+    for j in range(len(matrix) - 1, -1, -1):
+        crate = matrix[j][i * 4 + 1]
+        if crate != ' ': stack.append(crate)
+    stacks.append(stack)
+
+for instruction in instructions:
+    for i in range(instruction[0]):
+        crate = stacks[instruction[1]].pop(-1)
+        stacks[instruction[2]].append(crate)
+
+top_crates = ''.join([x[-1] for x in stacks])
+
+print(top_crates)
